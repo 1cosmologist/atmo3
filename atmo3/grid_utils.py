31c54e66aea4720b71a96ed1dac36b2a7e8fd0a0
Shamik Ghosh <thequarkexpress@gmail.com>
2026-04-10 00:33:28 -0700

    handles inequal box edges

--- atmo3/grid_utils.py
+++ atmo3/grid_utils.py
@@ -1,11 +1,12 @@
 import jax
+from functools import partial
 import jax.numpy as jnp
 import gc
 
 
 class GridWorkspace:
    
-    def __init__(self, N, Lbox, site_altitude=0.0, partition='jaxshard'):
+    def __init__(self, N, Lbox, site_altitude=0.0):
         """
         Initialize the grid workspace object.
 
@@ -53,52 +54,36 @@ class GridWorkspace:
         partaxis : int
             Axis to partition the array.
         """
-        self.N      = N
+        self.N     = N
+        
         self.Lbox   = Lbox
+        
         self.site_altitude = site_altitude  # Default altitude, can be set later if needed
+        
+        self.dk   = 2 * jnp.pi / self.Lbox
+        
+        if isinstance(self.dk, jnp.ndarray):
+            self.d3k = self.dk[0] * self.dk[1] * self.dk[2]
+        else:
+            self.d3k     = self.dk**3.
 
-        self.dk      = 2*jnp.pi/self.Lbox
-        self.d3k     = self.dk * self.dk * self.dk
-
-        self.rshape       = (self.N,self.N,self.N)
-        self.cshape       = (self.N,self.N,self.N//2+1)
-        self.rshape_local = (self.N,self.N,self.N)
-        self.cshape_local = (self.N,self.N,self.N//2+1)
-
-        self.start = 0
-        self.end   = self.N
+        if isinstance(self.N, jnp.ndarray):
+            self.rshape = (self.N[0], self.N[1], self.N[2])
+            self.cshape = (self.N[0], self.N[1], self.N[2]//2+1)
+        else:
+            self.rshape = (self.N, self.N, self.N)
+            self.cshape = (self.N, self.N, self.N//2+1)
         
         self.grid_spacing = self.Lbox / self.N
         
-        self.field = None
-
-        # needed for running on CPU with a single process
-        self.ngpus   = 1        
-        self.host_id = 0
-
-        self.parttype = partition
-
-        if self.parttype == 'jaxshard':
-            self.ngpus   = jax.device_count()
-            self.host_id = jax.process_index()
-            self.start   = self.host_id * self.N // self.ngpus
-            self.end     = (self.host_id + 1) * self.N // self.ngpus
-            
-            self.partaxis     = 1
-            self.rshape_local = (self.N, self.N // self.ngpus, self.N)
-            self.cshape_local = (self.N, self.N // self.ngpus, self.N // 2 + 1)
-            
-            
-    def k_axis(self, r=False, slab_axis=False):
-        if r: 
-            k_i = (jnp.fft.rfftfreq(self.N) * self.dk * self.N)#.astype(jnp.float32)
-        else:
-            k_i = (jnp.fft.fftfreq(self.N) * self.dk * self.N)#.astype(jnp.float32)
-        if slab_axis: return (k_i[self.start:self.end])#.astype(jnp.float32)
-        return k_i
+    
+    @partial(jax.jit, static_argnames=['self'])      
+    def k_axis(self, axis, r=False):
+        return jnp.where(r, (jnp.fft.rfftfreq(self.N[axis]) * self.dk[axis] * self.N[axis]), (jnp.fft.fftfreq(self.N[axis]) * self.dk[axis] * self.N[axis]))#.astype(jnp.float32)
     
     def k_square(self, kx, ky, kz):
         kxa,kya,kza = jnp.meshgrid(kx,ky,kz,indexing='ij')
+        
         del kx, ky, kz ; gc.collect()
 
         k2 = (kxa**2+kya**2+kza**2)#.astype(jnp.float32)
@@ -106,10 +91,11 @@ class GridWorkspace:
 
         return k2
     
+    @partial(jax.jit, static_argnames=['self'])
     def interp2kgrid(self, k_1d, f_1d):
-        kx = self.k_axis()
-        ky = self.k_axis(slab_axis=True)
-        kz = self.k_axis(r=True)
+        kx = self.k_axis(0)
+        ky = self.k_axis(1)
+        kz = self.k_axis(2, r=True)
 
         interp_fcn = jnp.sqrt(self.k_square(kx, ky, kz)).ravel()
         del kx, ky, kz ; gc.collect()
@@ -117,19 +103,16 @@ class GridWorkspace:
         interp_fcn = jnp.interp(interp_fcn, k_1d, f_1d, left=0., right='extrapolate')
         return jnp.reshape(interp_fcn, self.cshape_local)#.astype(jnp.float32)
     
-    def grid_axis(self, slab_axis=False, altitude_axis=False):
-        x_i = (jnp.arange(self.N) * self.grid_spacing)#.astype(jnp.float32)
+    @partial(jax.jit, static_argnames=['self'])
+    def grid_axis(self, axis, altitude_axis=False):
         
-        if altitude_axis:
-            x_i = x_i + self.site_altitude
-            
-        if slab_axis: return x_i[self.start:self.end]
-        return x_i
-    
+        return jnp.where(altitude_axis, self.site_altitude + (jnp.arange(self.N[axis]) * self.grid_spacing[axis]), (jnp.arange(self.N[axis]) * self.grid_spacing[axis]))#.astype(jnp.float32)
+        
+    @partial(jax.jit, static_argnames=['self'])
     def interp2grid(self, x_1d, f_1d):
-        x = self.grid_axis()
-        y = self.grid_axis(slab_axis=True)
-        z = self.grid_axis(altitude_axis=True)
+        x = self.grid_axis(0)
+        y = self.grid_axis(1)
+        z = self.grid_axis(2, altitude_axis=True)
 
         xx, yy, zz = jnp.meshgrid(x,y,z, indexing='ij')
 
@@ -137,4 +120,6 @@ class GridWorkspace:
 
         zz = zz.ravel()
         interp_fcn = jnp.interp(zz, x_1d, f_1d, left='extrapolate', right='extrapolate')
-        return jnp.reshape(interp_fcn, self.rshape_local)#.astype(jnp.float32)
+        return jnp.reshape(interp_fcn, self.rshape)#.astype(jnp.float32)
+    
+    
